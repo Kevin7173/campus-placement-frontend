f6fc322d2f6d4f4740f2bfea23caab00ebf2cbaf
Kevin M Koshy <kevinmkoshy@Kevins-MacBook-Air.local>
2026-07-04 17:37:25 +0530

    Fix auth API routes

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,12 +1,13 @@
 import API from "./axios";
 
 export const registerUser = (userData: any) =>
-  API.post("/auth/register", userData);
+  API.post("/api/auth/register", userData);
 
 export const loginUser = (userData: any) =>
-  API.post("/auth/login", userData);
+  API.post("/api/auth/login", userData);
 
 export const changePassword = (data: {
   currentPassword: string;
   newPassword: string;
-}) => API.put("/auth/change-password", data);
+}) =>
+  API.put("/api/auth/change-password", data);
